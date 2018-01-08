f54b337dc1ed57914c83dd2cb11718f8304ea496
Joachim Metz <joachim.metz@gmail.com>
2018-01-08 06:29:30 +0100

    Applied updates

--- pycaes/pycaes_crypt.h
+++ pycaes/pycaes_crypt.h
@@ -1,7 +1,7 @@
 /*
  * Python definition of the libcaes crypt functions
  *
- * Copyright (C) 2011-2017, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2011-2018, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
--- tests/pycaes_test_crypt_xts.py
+++ tests/pycaes_test_crypt_xts.py
@@ -2,7 +2,7 @@
 #
 # Python-bindings AES-XTS de/encryption testing program
 #
-# Copyright (C) 2011-2017, Joachim Metz <joachim.metz@gmail.com>
+# Copyright (C) 2011-2018, Joachim Metz <joachim.metz@gmail.com>
 #
 # Refer to AUTHORS for acknowledgements.
 #
